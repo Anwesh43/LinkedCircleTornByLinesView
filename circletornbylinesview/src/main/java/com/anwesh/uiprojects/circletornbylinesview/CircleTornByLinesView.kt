1330ef90f64c8994b81ae6f6caf23de0e7f2a00f
Anwesh43 <anweshthecool0@gmail.com>
2019-01-06 10:30:26 +0530

    adding CTLNode class

--- circletornbylinesview/src/main/java/com/anwesh/uiprojects/circletornbylinesview/CircleTornByLinesView.kt
+++ circletornbylinesview/src/main/java/com/anwesh/uiprojects/circletornbylinesview/CircleTornByLinesView.kt
@@ -121,4 +121,48 @@ class CircleTornByLinesView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CTLNode(var i : Int, val state : State = State()) {
+
+        private var next : CTLNode? = null
+        private var prev : CTLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CTLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCTLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CTLNode {
+            var curr : CTLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
