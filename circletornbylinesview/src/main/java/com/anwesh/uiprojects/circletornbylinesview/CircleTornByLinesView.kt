532e9c35c1f08f994d431c52eea46bcca1a267a0
Anwesh43 <anweshthecool0@gmail.com>
2019-01-06 10:02:25 +0530

    adding drawCTLNode canvas extension method

--- circletornbylinesview/src/main/java/com/anwesh/uiprojects/circletornbylinesview/CircleTornByLinesView.kt
+++ circletornbylinesview/src/main/java/com/anwesh/uiprojects/circletornbylinesview/CircleTornByLinesView.kt
@@ -18,6 +18,7 @@ val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
+val rSizeFactor : Float = 3.5f
 val foreColor : Int = Color.parseColor("#f44336")
 val backColor : Int = Color.parseColor("#BDBDBD")
 
@@ -27,3 +28,31 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawCTLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val rSize : Float = gap / rSizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.style = Paint.Style.STROKE
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.color = foreColor
+    val yGap : Float = rSize / (lines + 2)
+    save()
+    translate(w/2, gap * (i + 1))
+    rotate(90f * sc2)
+    drawCircle(0f, 0f, rSize, paint)
+    translate(-size, -size)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc1.divideScale(j, lines)
+        save()
+        translate(0f, yGap * (j + 1))
+        drawLine(0f, 0f, 2 * size * sc, 0f, paint)
+        restore()
+    }
+    restore()
+}
